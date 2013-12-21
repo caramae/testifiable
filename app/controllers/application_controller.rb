bcf4b0e1fc7899440e8a09bbed4c2c45216042c4
clintonjwang <clintonjwang@gmail.com>
2013-12-21 13:27:03 -0500

    fix heroku bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_filter :reset_session
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
