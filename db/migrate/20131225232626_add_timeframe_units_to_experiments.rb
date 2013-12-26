0cc2b90b56a1410a1b208dc674f5f7dd1feae44c
clintonjwang <clintonjwang@gmail.com>
2013-12-26 11:03:44 -0500

    fix heroku bug

--- db/migrate/20131225232626_add_timeframe_units_to_experiments.rb
+++ db/migrate/20131225232626_add_timeframe_units_to_experiments.rb
@@ -1,6 +1,7 @@
 class AddTimeframeUnitsToExperiments < ActiveRecord::Migration
   def change
     add_column :experiments, :timeframe_units, :string
-    change_column :experiments, :timeframe, :integer
+    remove_column :experiments, :timeframe, :string
+    add_column :experiments, :timeframe, :integer
   end
 end
